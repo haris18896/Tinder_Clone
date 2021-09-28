e3929aa52bc6ef6f18429e24eac81bd90d37e494
Haris Ahmad <haris18896@gmail.com>
2021-09-28 06:59:07 -0700

    Working with Header Component

--- tinder-clone/src/components/header/Header.js
+++ tinder-clone/src/components/header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Header.css';
 
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import IconButton from '@mui/material/IconButton';
+
+
 
 function Header() {
     return (
         <div className="header">
             <h2>Header</h2>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
         </div>
     )
 }
